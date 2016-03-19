4b8c78f5f93c01528b4375d75c9582fd54cc65e0
kensaku <kenken.rep.1ew@gmail.com>
2016-03-19 12:44:33 +0900

    mod sub print_xy to print_ave_of_xy in sub_routeine.pl from my_mac.

--- sub_routeine.pl
+++ sub_routeine.pl
@@ -1,11 +1,15 @@
 my ($x, $y) = (123, 456);
 
-print_xy($x, $y);
+print_ave_of_xy ($x, $y);
 
-sub print_xy {
+sub print_ave_of_xy {
 	my ($x, $y) = @_;
-	print "(\$x, \$y) = ($x, $y)";
+	print "(\$x, \$y) = ($x, $y)"."\n";
+	print calc_average($x, $y);
 	print "\n";
 }
 
-
+sub calc_average {
+	my ($x, $y) = @_;
+	return ($x + $y) / 2;
+}
